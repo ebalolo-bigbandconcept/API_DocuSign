d27de49115f8c6a32c072a7fc9fd9191b2eaf1f3
AnakinGig <gigantanakin19@gmail.com>
2025-12-10 14:42:18 +0100

    Keeping swagger ui and openapi 3

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_swagger_ui import get_swaggerui_blueprint
 from .docusign_api import docusign_bp
-from .swagger_setup import init_swagger
 import os
 
 def create_app():
@@ -20,7 +19,7 @@ def create_app():
   app.config["MAX_CONTENT_LENGTH"] = 10 * 1024 * 1024  # 10 MB
   
   app.register_blueprint(docusign_bp, url_prefix="/api")
-  init_swagger(app)
+  app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
   
   @app.route('/')
   def helloWord():
